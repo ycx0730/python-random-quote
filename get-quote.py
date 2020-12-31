f8ffe82a7f99f57df4ec4a395fffc66760641b97
Yap Chun Xiang <76789041+ycx0730@users.noreply.github.com>
2020-12-31 16:50:59 +0800

    Renamed the primary function

---
+++ get-quote.py
@@ -0,0 +1,11 @@
+def primary():
+  print("Keep it logically awesome.")
+
+  #f = open("quotes.txt")
+  #quotes = f.readlines()
+  #f.close()
+
+  #print(quotes)
+
+if __name__== "__main__":
+  primary()
